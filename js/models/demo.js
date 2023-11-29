053c2cd52a033aa2bf9a787c7564d10e6e7b6033
Shivam kumar <118986252+shivam6659@users.noreply.github.com>
2023-11-29 12:06:55 +0530

    Add files via upload

---
+++ js/models/demo.js
@@ -0,0 +1,11 @@
+// import {Schema, model, models} from "mangoose";
+
+// const demoSchema = new Schema ({
+//     name : String,
+//     date : Date,
+    
+// });
+
+// const demo = models.Demo || model("Demo", demoSchema);
+
+// export default Demo;
